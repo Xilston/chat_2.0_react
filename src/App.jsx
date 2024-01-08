b0f32a397eec53038506b440588f1bcce89e83e4
Yaroslav Samoilenko <yasik2255@live.com>
2024-01-08 21:01:31 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,44 @@
-import 'bulma/css/bulma.css';
+/* eslint-disable */
+
+// import 'bulma/css/bulma.css';
 import './App.scss';
 import { useState } from 'react';
 import cn from 'classnames';
 
-export const goods = [
-  'Dumplings',
-  'Carrot',
-  'Eggs',
-  'Ice cream',
-  'Apple',
-  'Bread',
-  'Fish',
-  'Honey',
-  'Jam',
-  'Garlic',
+import { Header } from './Header';
+import './styles/index.scss';
+import { Main } from './Main';
+import { Footer } from './Footer';
+
+const messages = [
+  {author: 'Vlad', body: 'hih ih hi everyone',},
+  {author: 'Mari', body: 'hih ih hi everyone',},
+  {author: 'Yaro', body: 'hi5555 fgdfgdfge',},
+  {author: 'Vlad', body: 'jenshini ne ludi',},
+  {author: 'Den', body: 'hpatriarhat forevere',},
+  {author: 'Xilston petuh', body: 'hfuck lgbt',},
 ];
 
-const START_SELECTED_GOOD = 'Jam';
+const dialogs = [
+  {title: 'GLOBAL ROOM', active: true},
+  {title: 'JOHN'},
+  {title: 'CUSTOM ROOM'},
+  {title: 'FILIP'},
+  {title: 'MARIA'},
+  {title: 'Den'},
+  {title: 'Yaro'},
+];
 
 export const App = () => {
-  const [selectedGood, setSelectedGood] = useState(START_SELECTED_GOOD);
-
-  const selectGood = (name) => {
-    const callback = () => setSelectedGood(name);
-
-    return callback;
-  };
-
-  const unSelectGood = () => {
-    setSelectedGood('');
-  };
+  const ulala = 11;
 
   return (
-    <main className="section container">
-
-      { selectedGood ? (
-        <h1 className="title is-flex is-align-items-center">
-          {`${selectedGood} is selected`}
-
-          <button
-            data-cy="ClearButton"
-            type="button"
-            className="delete ml-3"
-            onClick={unSelectGood}
-          />
-        </h1>
-      ) : (
-        <h1 className="title is-flex is-align-items-center">
-          No goods selected
-        </h1>
-      )
-      }
-
-      <table className="table">
-        <tbody>
-          {
-            goods.map(
-              (good) => {
-                const isSelected = good === selectedGood;
-
-                let button = (
-                  <button
-                    data-cy="AddButton"
-                    type="button"
-                    className="button"
-                    onClick={selectGood(good)}
-                  >
-                    +
-                  </button>
-                );
-
-                if (isSelected) {
-                  button = (
-                    <button
-                      data-cy="RemoveButton"
-                      type="button"
-                      className="button is-info"
-                      onClick={unSelectGood}
-                    >
-                      -
-                    </button>
-                  );
-                }
-
-                return (
-                  <tr
-                    key={good}
-                    data-cy="Good"
-                    className={
-                      cn({ 'has-background-success-light': isSelected })
-                    }
-                  >
-                    <td>
-                      { button }
-                    </td>
-
-                    <td data-cy="GoodTitle" className="is-vcentered">
-                      { good }
-                    </td>
-                  </tr>
-                );
-              },
-            )
-          }
-        </tbody>
-      </table>
-    </main>
+    <>
+      <Header />
+      <Main messages={messages} dialogs={dialogs} />
+      <Footer />
+    </>
   );
 };
+
+
